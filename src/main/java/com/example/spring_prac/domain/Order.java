8472c7039b898b3b68e087671d6c3e0a1e17208e
jin <yjj1544@gmail.com>
2026-01-30 17:35:54 +0900

    필수 과제 및 도전 과제 중 주문 목록 조회까지 작업

---
+++ src/main/java/com/example/spring_prac/domain/Order.java
@@ -0,0 +1,22 @@
+package com.example.spring_prac.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="productId",nullable = false)
+    private Product product;
+
+}
